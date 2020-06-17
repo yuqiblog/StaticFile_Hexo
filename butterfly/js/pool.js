a480116ca95f60fe27e595cf2bedd886e432fc02
Antmoe <sviptzk@163.com>
2020-06-17 10:31:08 +0800

    更新蝴蝶3.0版本

--- butterfly/js/pool.js
+++ butterfly/js/pool.js
@@ -1,111 +1,91 @@
 /*
-    更换壁纸
+    cookie更换壁纸
 */
-// 获取标签
-// 全局背景div
-var web_bg = document.getElementById("web_bg");
-// 公共父级
-var content_inner = document.getElementById("content-inner");
-// 获取Cookies
-// 透明度
-var opacity = Cookies.get("opacity");
-// 背景
-var bg = Cookies.get("bg");
-// 动画
-var animation = Cookies.get("animation");
-// 背景类型
-var type = Cookies.get("type");
-// 声明遍历 用于记录当前color
-// 设置背景
-if (bg) {
-  web_bg.style.background = bg;
-  web_bg.setAttribute("data-type", type);
-  if (animation) {
-    web_bg.style.animation = animation;
+(function () {
+  // 获取标签
+  // 全局背景div
+  var web_bg = document.getElementById("web_bg");
+  // 公共父级
+  var content_inner = document.getElementById("content-inner");
+  // 获取Cookies
+  // 透明度
+  var opacity = Cookies.get("opacity");
+  // 背景
+  var bg = Cookies.get("bg");
+  // 动画
+  var animation = Cookies.get("animation");
+  // 背景类型
+  var type = Cookies.get("type");
+  // 声明遍历 用于记录当前color
+  // 设置背景
+  if (bg) {
+    web_bg.style.background = bg;
+    web_bg.setAttribute("data-type", type);
+    if (animation) {
+      web_bg.style.animation = animation;
+    }
   }
-}
-function setColor(opacity) {
-  // style="--light_bg_color: rgb(255, 255, 255,.3);--dark_bg_color: rgba(18,18,18,.2);"
-  var light_bg_color = "--light_bg_color: rgb(255, 255, 255," + opacity + ");";
-  var dark_bg_color = "--dark_bg_color: rgba(18,18,18," + opacity + ");";
-  content_inner.setAttribute("style", light_bg_color + dark_bg_color);
-}
-setColor(opacity);
-/*
+  function setColor(opacity) {
+    // style="--light_bg_color: rgb(255, 255, 255,.3);--dark_bg_color: rgba(18,18,18,.2);"
+    var light_bg_color =
+      "--light_bg_color: rgb(255, 255, 255," + opacity + ");";
+    var dark_bg_color = "--dark_bg_color: rgba(18,18,18," + opacity + ");";
+    content_inner.setAttribute("style", light_bg_color + dark_bg_color);
+  }
+  setColor(opacity);
+  /*
     手机侧边默认隐藏
 */
-var mobile_sidebar_menus = document.getElementById("mobile-sidebar-menus");
-var menus_item_child = mobile_sidebar_menus.getElementsByClassName(
-  "menus_item_child"
-);
-var menus_expand = mobile_sidebar_menus.getElementsByClassName("menus-expand");
-for (var i = 0; i < menus_item_child.length; i++) {
-  menus_item_child[i].style.display = "none";
-  menus_expand[i].className += " menus-closed";
-}
-/* 
-  分类卡片隐藏
-*/
-var card_category_list = document.getElementsByClassName(
-  "card-category-list child"
-);
-var item = document.getElementsByClassName("card-category-list-item");
-function toggle(t) {
-  var display = t.parentElement.nextSibling.style.display;
-  if (display == "none") {
-    t.parentElement.nextSibling.style.display = "block";
-    t.parentElement.nextSibling.style.height = "100%";
-    t.className = t.className.replace("fa-chevron-up", "fa-chevron-down");
-  } else {
-    t.parentElement.nextSibling.style.display = "none";
-    t.className = t.className.replace("fa-chevron-down", "fa-chevron-up");
+  var mobile_sidebar_menus = document.getElementById("mobile-sidebar-menus");
+  var menus_item_child = mobile_sidebar_menus.getElementsByClassName(
+    "menus_item_child"
+  );
+  var menus_expand = mobile_sidebar_menus.getElementsByClassName(
+    "menus-expand"
+  );
+  for (var i = 0; i < menus_item_child.length; i++) {
+    menus_item_child[i].style.display = "none";
+    menus_expand[i].className += " menus-closed";
   }
-}
+})();
 
-for (var i = 0; i < card_category_list.length; i++) {
-  card_category_list[i].style.display = "none";
-  card_category_list[i].style.transition = "all 1s";
-  card_category_list[i].previousSibling.innerHTML +=
-    '<i class="fa fa-chevron-up menus-expand  menus-closed" aria-hidden="true" style="margin-left:20px;" onclick="toggle(this)"></i>';
-}
 /*
     扩展js
 */
-/* eslint-disable */
-(function ($) {
+// tab默认行为
+(function (t) {
   "use strict";
-  function setTabs() {
-    const $tabs = $(".tabs");
-    if ($tabs.length === 0) return;
-    let $navs = $tabs.find(".nav-tabs .tab");
-    for (var i = 0; i < $navs.length; i++) {
-      let $a = $tabs.find($navs[i].children[0]);
-      $a.addClass($a.attr("href"));
-      $a.removeAttr("href");
+  function e() {
+    const e = t(".tabs");
+    if (0 === e.length) return;
+    let i = e.find(".nav-tabs .tab");
+    for (var n = 0; n < i.length; n++) {
+      let t = e.find(i[n].children[0]);
+      t.addClass(t.attr("href")), t.removeAttr("href");
     }
-    $(".tabs .nav-tabs").on("click", "a", (e) => {
-      e.preventDefault();
-      e.stopPropagation();
-      let $tab = $(e.target.parentElement.parentElement.parentElement);
-      $tab.find(".nav-tabs .active").removeClass("active");
-      $tab.find(e.target.parentElement).addClass("active");
-      $tab.find(".tab-content .active").removeClass("active");
-      $tab.find($(e.target).attr("class")).addClass("active");
-      return false;
+    t(".tabs .nav-tabs").on("click", "a", (e) => {
+      e.preventDefault(), e.stopPropagation();
+      let i = t(e.target.parentElement.parentElement.parentElement);
+      return (
+        i.find(".nav-tabs .active").removeClass("active"),
+        i.find(e.target.parentElement).addClass("active"),
+        i.find(".tab-content .active").removeClass("active"),
+        i.find(t(e.target).attr("class")).addClass("active"),
+        !1
+      );
     });
   }
-
-  $(function () {
-    setTabs();
-    // $(".article .video-container").fitVids();
-    $(".scroll-down").on("click", function () {
-      scrolltoElement(".l_body");
-    });
-    setTimeout(function () {
-      $("#loading-bar-wrapper").fadeOut(500);
-    }, 300);
+  t(function () {
+    e(),
+      t(".scroll-down").on("click", function () {
+        scrolltoElement(".l_body");
+      }),
+      setTimeout(function () {
+        t("#loading-bar-wrapper").fadeOut(500);
+      }, 300);
   });
 })(jQuery);
+
 /*
     轮播图JS
 */
@@ -321,15 +301,14 @@ if (commentElement != undefined) {
 $("#switch-comment").click(function () {
   return switchComment(), !1;
 });
-
 // 修改Banner
-var full_page = document.getElementsByClassName("full_page");
-
-if (full_page.length > 0) {
-  full_page[0].style.backgroundImage =
-    'url("https://ae01.alicdn.com/kf/H18a4b998752a4ae68b8e85d432a5aef0l.png"), linear-gradient(60deg, rgba(255, 165, 150, 0.5) 5%, rgba(0, 228, 255, 0.35)), url("https://ae01.alicdn.com/kf/H21b5f6b8496141a1979a33666e1074d9x.jpg")';
-}
-
+(function () {
+  var full_page = document.getElementsByClassName("full_page");
+  if (full_page.length > 0) {
+    full_page[0].style.backgroundImage =
+      'url("https://ae01.alicdn.com/kf/H18a4b998752a4ae68b8e85d432a5aef0l.png"), linear-gradient(60deg, rgba(255, 165, 150, 0.5) 5%, rgba(0, 228, 255, 0.35)), url("https://ae01.alicdn.com/kf/H21b5f6b8496141a1979a33666e1074d9x.jpg")';
+  }
+})();
 /*
     其他
 */
--- butterfly/js/search/local-search.js
+++ butterfly/js/search/local-search.js
@@ -1,174 +1,152 @@
 $(function () {
-  var loadFlag = false;
-  $("a.social-icon.search").on("click", function () {
-    $("body").css({
-      width: "100%",
-      overflow: "hidden",
-    });
-    $(".search-dialog").css("display", "block");
-    $("#local-search-input input").focus();
-    $(".search-mask").fadeIn();
+  var loadFlag = false
+  $('a.social-icon.search').on('click', function () {
+    $('body').css({
+      width: '100%',
+      overflow: 'hidden'
+    })
+    $('.search-dialog').css('display', 'block')
+    $('#local-search-input input').focus()
+    $('.search-mask').fadeIn()
     if (!loadFlag) {
-      search(GLOBAL_CONFIG.localSearch.path);
-      loadFlag = true;
+      search(GLOBAL_CONFIG.localSearch.path)
+      loadFlag = true
     }
 
     // shortcut: ESC
-    document.addEventListener("keydown", function f(event) {
-      if (event.code === "Escape") {
-        closeSearch();
-        document.removeEventListener("keydown", f);
+    document.addEventListener('keydown', function f (event) {
+      if (event.code === 'Escape') {
+        closeSearch()
+        document.removeEventListener('keydown', f)
       }
-    });
-  });
+    })
+  })
 
   var closeSearch = function () {
-    $("body").css("width", "");
-    $("body").css("overflow", "");
-    $(".search-dialog").css({
-      animation: "search_close .5s",
-    });
+    $('body').css('width', '')
+    $('body').css('overflow', '')
+    $('.search-dialog').css({
+      animation: 'search_close .5s'
+    })
 
-    $(".search-dialog").animate({}, function () {
+    $('.search-dialog').animate({}, function () {
       setTimeout(function () {
-        $(".search-dialog").css({
-          animation: "",
-          display: "none",
-        });
-      }, 500);
-    });
+        $('.search-dialog').css({
+          animation: '',
+          display: 'none'
+        })
+      }, 500)
+    })
 
-    $(".search-mask").fadeOut();
-  };
-  $(".search-mask, .search-close-button").on("click touchstart", closeSearch);
+    $('.search-mask').fadeOut()
+  }
+  $('.search-mask, .search-close-button').on('click touchstart', closeSearch)
 
-  function search(path) {
+  function search (path) {
     $.ajax({
-      // url: GLOBAL_CONFIG.root + path,
-      url: "https://cdn.jsdelivr.net/gh/sviptzk/sviptzk.github.io/search.xml",
-      dataType: "xml",
+      url: GLOBAL_CONFIG.root + path,
+      dataType: 'xml',
       success: function (xmlResponse) {
         // get the contents from search data
-        var datas = $("entry", xmlResponse)
-          .map(function () {
-            return {
-              title: $("title", this).text(),
-              content: $("content", this).text(),
-              url: $("url", this).text(),
-            };
-          })
-          .get();
-
-        var $input = $("#local-search-input input")[0];
-        var $resultContent = $("#local-hits")[0];
-        $input.addEventListener("input", function () {
-          var str = '<div class="search-result-list">';
-          var keywords = this.value.trim().toLowerCase().split(/[\s]+/);
-          $resultContent.innerHTML = "";
-          if (this.value.trim().length <= 0) {
-            $(".local-search-stats__hr").hide();
-            return;
+        var datas = $('entry', xmlResponse).map(function () {
+          return {
+            title: $('title', this).text(),
+            content: $('content', this).text(),
+            url: $('url', this).text()
           }
-          var count = 0;
+        }).get()
+
+        var $input = $('#local-search-input input')[0]
+        var $resultContent = $('#local-hits')[0]
+        $input.addEventListener('input', function () {
+          var str = '<div class="search-result-list">'
+          var keywords = this.value.trim().toLowerCase().split(/[\s]+/)
+          $resultContent.innerHTML = ''
+          if (this.value.trim().length <= 0) {
+            $('.local-search-stats__hr').hide()
+            return
+          }
+          var count = 0
           // perform local searching
           datas.forEach(function (data) {
-            var isMatch = true;
-            if (!data.title || data.title.trim() === "") {
-              data.title = "Untitled";
+            var isMatch = true
+            if (!data.title || data.title.trim() === '') {
+              data.title = 'Untitled'
             }
-            var dataTitle = data.title.trim().toLowerCase();
-            var dataContent = data.content
-              .trim()
-              .replace(/<[^>]+>/g, "")
-              .toLowerCase();
-            var dataUrl = data.url;
-            var indexTitle = -1;
-            var indexContent = -1;
-            var firstOccur = -1;
+            var dataTitle = data.title.trim().toLowerCase()
+            var dataContent = data.content.trim().replace(/<[^>]+>/g, '').toLowerCase()
+            var dataUrl = data.url
+            var indexTitle = -1
+            var indexContent = -1
+            var firstOccur = -1
             // only match artiles with not empty titles and contents
-            if (dataTitle !== "" || dataContent !== "") {
+            if (dataTitle !== '' || dataContent !== '') {
               keywords.forEach(function (keyword, i) {
-                indexTitle = dataTitle.indexOf(keyword);
-                indexContent = dataContent.indexOf(keyword);
+                indexTitle = dataTitle.indexOf(keyword)
+                indexContent = dataContent.indexOf(keyword)
                 if (indexTitle < 0 && indexContent < 0) {
-                  isMatch = false;
+                  isMatch = false
                 } else {
                   if (indexContent < 0) {
-                    indexContent = 0;
+                    indexContent = 0
                   }
                   if (i === 0) {
-                    firstOccur = indexContent;
+                    firstOccur = indexContent
                   }
                 }
-              });
+              })
             } else {
-              isMatch = false;
+              isMatch = false
             }
 
             // show search results
             if (isMatch) {
-              var content = data.content.trim().replace(/<[^>]+>/g, "");
+              var content = data.content.trim().replace(/<[^>]+>/g, '')
               if (firstOccur >= 0) {
                 // cut out 130 characters
-                var start = firstOccur - 30;
-                var end = firstOccur + 100;
+                var start = firstOccur - 30
+                var end = firstOccur + 100
 
                 if (start < 0) {
-                  start = 0;
+                  start = 0
                 }
 
                 if (start === 0) {
-                  end = 100;
+                  end = 100
                 }
 
                 if (end > content.length) {
-                  end = content.length;
+                  end = content.length
                 }
 
-                var matchContent = content.substring(start, end);
+                var matchContent = content.substring(start, end)
 
                 // highlight all keywords
                 keywords.forEach(function (keyword) {
-                  var regS = new RegExp(keyword, "gi");
-                  matchContent = matchContent.replace(
-                    regS,
-                    '<span class="search-keyword">' + keyword + "</span>"
-                  );
-                  dataTitle = dataTitle.replace(
-                    regS,
-                    '<span class="search-keyword">' + keyword + "</span>"
-                  );
-                });
+                  var regS = new RegExp(keyword, 'gi')
+                  matchContent = matchContent.replace(regS, '<span class="search-keyword">' + keyword + '</span>')
+                  dataTitle = dataTitle.replace(regS, '<span class="search-keyword">' + keyword + '</span>')
+                })
 
-                str +=
-                  '<div class="local-search__hit-item"><a href="' +
-                  dataUrl +
-                  '" class="search-result-title">' +
-                  dataTitle +
-                  "</a>";
-                count += 1;
-                $(".local-search-stats__hr").show();
+                str += '<div class="local-search__hit-item"><a href="' + dataUrl + '" class="search-result-title">' + dataTitle + '</a>'
+                count += 1
+                $('.local-search-stats__hr').show()
 
-                if (dataContent !== "") {
-                  str += '<p class="search-result">' + matchContent + "...</p>";
+                if (dataContent !== '') {
+                  str += '<p class="search-result">' + matchContent + '...</p>'
                 }
               }
-              str += "</div>";
+              str += '</div>'
             }
-          });
+          })
           if (count === 0) {
-            str +=
-              '<div id="local-search__hits-empty">' +
-              GLOBAL_CONFIG.localSearch.languages.hits_empty.replace(
-                /\$\{query}/,
-                this.value.trim()
-              ) +
-              "</div>";
+            str += '<div id="local-search__hits-empty">' + GLOBAL_CONFIG.localSearch.languages.hits_empty.replace(/\$\{query}/, this.value.trim()) +
+              '</div>'
           }
-          str += "</div>";
-          $resultContent.innerHTML = str;
-        });
-      },
-    });
+          str += '</div>'
+          $resultContent.innerHTML = str
+        })
+      }
+    })
   }
-});
+})
